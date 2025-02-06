3d95a4897ed118d62878f431b17d2e4b1835b486
Eyad Laza <eyadlaza@gmail.com>
2025-02-06 20:57:18 +0100

    14 Good

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,10 +1,13 @@
-export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "NxtAmzn";
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "EmiratesPlaza";
 export const APP_SLOGAN =
   process.env.NEXT_PUBLIC_APP_SLOGAN || "Spend less, enjoy more.";
 export const APP_DESCRIPTION =
   process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
-  "An Amazon clone built with Next.js and MongoDB";
+  "EmiratesPlaza is a leading online store for all your shopping needs.";
 export const PAGE_SIZE = Number(process.env.PAGE_SIZE || 9);
 export const FREE_SHIPPING_MIN_PRICE = Number(
   process.env.FREE_SHIPPING_MIN_PRICE || 35
 );
+export const APP_COPYRIGHT =
+  process.env.NEXT_PUBLIC_APP_COPYRIGHT ||
+  `Copyright © 2025 ${APP_NAME}. All rights reserved.`;
