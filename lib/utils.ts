61da826a1bb9a5bc6912caa37a8ab31badcd3a45
Eyad Laza <eyadlaza@gmail.com>
2025-02-06 21:19:57 +0100

    16 finish

--- lib/utils.ts
+++ lib/utils.ts
@@ -28,7 +28,7 @@ export const toSlug = (text: string): string =>
     .replace(/\s+/g, "-")
     .replace(/^-+|-+$/g, "")
     .replace(/-+/g, "-")
-    
+
 const CURRENCY_FORMATTER = new Intl.NumberFormat("en-US", {
   currency: "USD",
   style: "currency",
@@ -37,4 +37,28 @@ const CURRENCY_FORMATTER = new Intl.NumberFormat("en-US", {
 
 export function formatCurrency(amount: number) {
   return CURRENCY_FORMATTER.format(amount);
+}
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const formatError = (error: any): string => {
+  if (error.name === 'ZodError') {
+    const fieldErrors = Object.keys(error.errors).map((field) => {
+      const errorMessage = error.errors[field].message
+      return `${error.errors[field].path}: ${errorMessage}` // field: errorMessage
+    })
+    return fieldErrors.join('. ')
+  } else if (error.name === 'ValidationError') {
+    const fieldErrors = Object.keys(error.errors).map((field) => {
+      const errorMessage = error.errors[field].message
+      return errorMessage
+    })
+    return fieldErrors.join('. ')
+  } else if (error.code === 11000) {
+    const duplicateField = Object.keys(error.keyValue)[0]
+    return `${duplicateField} already exists`
+  } else {
+    // return 'Something went wrong. please try again'
+    return typeof error.message === 'string'
+      ? error.message
+      : JSON.stringify(error.message)
+  }
 }
