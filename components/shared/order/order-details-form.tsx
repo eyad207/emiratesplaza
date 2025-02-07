d50c19e99efa5521e22b8a8b382f961008eba698
Eyad Laza <eyadlaza@gmail.com>
2025-02-07 21:52:30 +0100

    31 Good

--- components/shared/order/order-details-form.tsx
+++ components/shared/order/order-details-form.tsx
@@ -14,12 +14,14 @@ import {
   TableRow,
 } from '@/components/ui/table'
 import { IOrder } from '@/lib/db/models/order.model'
-import { cn, formatDateTime } from '@/lib/utils'
-import { buttonVariants } from '@/components/ui/button'
+import { formatDateTime } from '@/lib/utils'
 import ProductPrice from '../product/product-price'
+import ActionButton from '../action-button'
+import { deliverOrder, updateOrderToPaid } from '@/lib/actions/order.actions'
 
 export default function OrderDetailsForm({
   order,
+  isAdmin,
 }: {
   order: IOrder
   isAdmin: boolean
@@ -153,14 +155,17 @@ export default function OrderDetailsForm({
                 <ProductPrice price={totalPrice} plain />
               </div>
             </div>
-
-            {!isPaid && ['Stripe', 'PayPal'].includes(paymentMethod) && (
-              <Link
-                className={cn(buttonVariants(), 'w-full')}
-                href={`/checkout/${order._id}`}
-              >
-                Pay Order
-              </Link>
+            {isAdmin && !isPaid && paymentMethod === 'Cash On Delivery' && (
+              <ActionButton
+                caption='Mark as paid'
+                action={() => updateOrderToPaid(order._id)}
+              />
+            )}
+            {isAdmin && isPaid && !isDelivered && (
+              <ActionButton
+                caption='Mark as delivered'
+                action={() => deliverOrder(order._id)}
+              />
             )}
           </CardContent>
         </Card>
