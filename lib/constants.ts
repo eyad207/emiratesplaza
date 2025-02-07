468073eedd9b16a9eb238cb08d46b185283dbeee
Eyad Laza <eyadlaza@gmail.com>
2025-02-07 16:51:42 +0100

    19 Good

--- lib/constants.ts
+++ lib/constants.ts
@@ -12,38 +12,44 @@ export const APP_COPYRIGHT =
   process.env.NEXT_PUBLIC_APP_COPYRIGHT ||
   `Copyright © 2025 ${APP_NAME}. All rights reserved.`;
 
-  export const AVAILABLE_PAYMENT_METHODS = [
-    {
-      name: "PayPal",
-      commission: 0,
-      isDefault: true,
-    },
-    {
-      name: "Stripe",
-      commission: 0,
-      isDefault: false,
-    },
-  ];
-  export const DEFAULT_PAYMENT_METHOD =
-    process.env.DEFAULT_PAYMENT_METHOD || "PayPal";
-  
-  export const AVAILABLE_DELIVERY_DATES = [
-    {
-      name: "Tomorrow",
-      daysToDeliver: 1,
-      shippingPrice: 12.9,
-      freeShippingMinPrice: 0,
-    },
-    {
-      name: "Next 3 Days",
-      daysToDeliver: 3,
-      shippingPrice: 6.9,
-      freeShippingMinPrice: 0,
-    },
-    {
-      name: "Next 5 Days",
-      daysToDeliver: 5,
-      shippingPrice: 4.9,
-      freeShippingMinPrice: 35,
-    },
-  ];
+export const AVAILABLE_PAYMENT_METHODS = [
+  {
+    name: "PayPal",
+    commission: 0,
+    isDefault: true,
+  },
+  {
+    name: "Stripe",
+    commission: 0,
+    isDefault: false,
+  },
+];
+export const DEFAULT_PAYMENT_METHOD =
+  process.env.DEFAULT_PAYMENT_METHOD || "PayPal";
+
+export const AVAILABLE_DELIVERY_DATES = [
+  {
+    name: "Tomorrow",
+    daysToDeliver: 1,
+    shippingPrice: 12.9,
+    freeShippingMinPrice: 0,
+  },
+  {
+    name: "Next 3 Days",
+    daysToDeliver: 3,
+    shippingPrice: 6.9,
+    freeShippingMinPrice: 0,
+  },
+  {
+    name: "Next 5 Days",
+    daysToDeliver: 5,
+    shippingPrice: 4.9,
+    freeShippingMinPrice: 35,
+  },
+];
+
+export const SERVER_URL =
+  process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:3000";
+
+export const SENDER_EMAIL = process.env.SENDER_EMAIL || "onboarding@resend.dev";
+export const SENDER_NAME = process.env.SENDER_NAME || APP_NAME;
