af7cc233cb1b9fc280c261f8acb16f5eb7f5eca1
Eyad Laza <eyadlaza@gmail.com>
2025-02-24 16:35:46 +0100

    feat: update available payment methods to specify 'PayPal' instead of 'PayPal / Credit Card'

--- lib/data.ts
+++ lib/data.ts
@@ -1613,8 +1613,9 @@ We also provide helpful resources such as order tracking, product guides, and FA
       ],
       defaultCurrency: 'USD',
       availablePaymentMethods: [
-        { name: 'PayPal / Credit Card', commission: 0 },
+        { name: 'PayPal', commission: 0 },
         { name: 'Stripe', commission: 0 },
+        { name: 'Cash On Delivery', commission: 0 },
       ],
       defaultPaymentMethod: 'PayPal',
       availableDeliveryDates: [
