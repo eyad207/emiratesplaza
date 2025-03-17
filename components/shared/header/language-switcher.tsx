8c37f9bed6fef9184aa41c3a381d6d5a8b226f1b
Eyad Laza <eyadlaza@gmail.com>
2025-03-17 11:43:14 +0100

    feat: update LanguageSwitcher to display currency symbol and default to AED; remove language selection functionality

--- components/shared/header/language-switcher.tsx
+++ components/shared/header/language-switcher.tsx
@@ -7,59 +7,38 @@ import {
   DropdownMenuLabel,
   DropdownMenuRadioGroup,
   DropdownMenuRadioItem,
-  DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu'
-
-import { useLocale } from 'next-intl'
-import { Link, usePathname } from '@/i18n/routing'
 import useSettingStore from '@/hooks/use-setting-store'
-import { i18n } from '@/i18n-config'
 import { setCurrencyOnServer } from '@/lib/actions/setting.actions'
 import { ChevronDownIcon } from 'lucide-react'
 
 export default function LanguageSwitcher() {
-  const { locales } = i18n
-  const locale = useLocale()
-  const pathname = usePathname()
-
   const {
-    setting: { availableCurrencies, currency },
+    setting: { availableCurrencies, currency = 'AED' }, // Set default currency to AED
     setCurrency,
   } = useSettingStore()
   const handleCurrencyChange = async (newCurrency: string) => {
     await setCurrencyOnServer(newCurrency)
     setCurrency(newCurrency)
   }
+
+  const currentCurrency = availableCurrencies.find((c) => c.code === currency)
+
+  const displaySymbol =
+    currentCurrency?.code === 'AED' ? 'د.إ' : currentCurrency?.symbol
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger className='header-button h-[41px]'>
         <div className='flex items-center gap-1'>
-          <span className='text-xl'>
-            {locales.find((l) => l.code === locale)?.icon}
+          <span className='text-l'>
+            {displaySymbol} {currency}
           </span>
-          {locale.toUpperCase().slice(0, 2)}
           <ChevronDownIcon />
         </div>
       </DropdownMenuTrigger>
       <DropdownMenuContent className='w-56'>
-        <DropdownMenuLabel>Language</DropdownMenuLabel>
-        <DropdownMenuRadioGroup value={locale}>
-          {locales.map((c) => (
-            <DropdownMenuRadioItem key={c.name} value={c.code}>
-              <Link
-                className='w-full flex items-center gap-1'
-                href={pathname}
-                locale={c.code}
-              >
-                <span className='text-lg'>{c.icon}</span> {c.name}
-              </Link>
-            </DropdownMenuRadioItem>
-          ))}
-        </DropdownMenuRadioGroup>
-
-        <DropdownMenuSeparator />
-
         <DropdownMenuLabel>Currency</DropdownMenuLabel>
         <DropdownMenuRadioGroup
           value={currency}
@@ -67,7 +46,7 @@ export default function LanguageSwitcher() {
         >
           {availableCurrencies.map((c) => (
             <DropdownMenuRadioItem key={c.name} value={c.code}>
-              {c.symbol} {c.code}
+              {c.code === 'AED' ? 'د.إ' : c.symbol} {c.code}
             </DropdownMenuRadioItem>
           ))}
         </DropdownMenuRadioGroup>
